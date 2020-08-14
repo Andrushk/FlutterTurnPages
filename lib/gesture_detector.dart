09091efbe94cd8525c4e0e8449110201b1d8298f
Andrey <andrushk_nn@mail.ru>
2020-08-14 14:00:32 +0300

    анимация листания

--- lib/gesture_detector.dart
+++ lib/gesture_detector.dart
@@ -74,11 +74,14 @@ class SwipeStack extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    RenderBox box = context.findRenderObject();
+
     return Stack(
+      overflow: Overflow.visible,
       children: [
         bottomPage ?? Container(),
         StreamBuilder<int>(
-          stream: controller.pageNumber,
+          stream: controller.topPageNumber,
           builder: (context, snapshot) {
             return IndexedStack(
               children: children,
@@ -86,13 +89,32 @@ class SwipeStack extends StatelessWidget {
             );
           },
         ),
+        StreamBuilder<FlyingPageData>(
+          stream: controller.flyingPage,
+          builder: (context, snapshot) {
+            return snapshot.data == null
+                ? Container()
+                : Positioned(
+                    left: snapshot.data.x,
+                    top: 0,
+                    width: box.size.width,
+                    height: box.size.height,
+                    child: Transform.rotate(
+                      angle: snapshot.data.angle,
+                      child: children[snapshot.data.pageIndex],
+                    ),
+                  );
+          },
+        ),
         GestureDetector(
           onHorizontalDragStart: (details) {
-            RenderBox box = context.findRenderObject();
             controller.start(box.size, details.localPosition);
           },
           onHorizontalDragUpdate: (details) {
             controller.move(details.localPosition);
+          },
+          onHorizontalDragEnd: (details) {
+            controller.cancel();
           },
           onHorizontalDragCancel: () {
             controller.cancel();
@@ -103,6 +125,17 @@ class SwipeStack extends StatelessWidget {
   }
 }
 
+class FlyingPageData {
+  final double x;
+  final double angle;
+  final int pageIndex;
+
+  FlyingPageData(this.x, this.angle, this.pageIndex);
+}
+
+const xSpeedFactor = 2.0;
+const angleSpeedFactor = 6.0;
+
 class HorizontalSwipeController {
   double _startAtX;
   double _sizeToSwitchX;
@@ -110,9 +143,12 @@ class HorizontalSwipeController {
   int currentPage = 0;
   List<Widget> _items = [];
 
-  final pageNumberController = BehaviorSubject<int>();
+  final topPageNumberController = BehaviorSubject<int>();
+  final flyingPageController = BehaviorSubject<FlyingPageData>();
 
-  Stream<int> get pageNumber => pageNumberController.stream;
+  Stream<int> get topPageNumber => topPageNumberController.stream;
+
+  Stream<FlyingPageData> get flyingPage => flyingPageController.stream;
 
   _setItems(List<Widget> items) {
     _items = items;
@@ -125,10 +161,24 @@ class HorizontalSwipeController {
 
   move(Offset currentPosition) {
     if (_startAtX == null || _sizeToSwitchX == null) return;
-    _position =
-        ((currentPosition.dx - _startAtX) / _sizeToSwitchX).clamp(-1.0, 1.0);
+    final dx = currentPosition.dx - _startAtX;
+    _position = (dx / _sizeToSwitchX).clamp(-1.0, 1.0);
 
-    //print('offset = $_position');
+    if (_position > 0) {
+      flyingPageController.add(FlyingPageData(
+        (dx - _sizeToSwitchX) * xSpeedFactor,
+        (_position-1) / angleSpeedFactor,
+        0,
+      ));
+    } else if (_position<0) {
+      flyingPageController.add(FlyingPageData(
+        dx * xSpeedFactor,
+        _position / angleSpeedFactor,
+        0,
+      ));
+    }
+
+    //print('dx=$dx, offset = $_position');
 
     if (_position >= 1) {
       cancel();
@@ -144,26 +194,29 @@ class HorizontalSwipeController {
   nextPage() {
     if (currentPage <= 0) return;
     currentPage--;
-    pageNumberController.add(currentPage);
+    topPageNumberController.add(currentPage);
     print('next');
   }
 
   prevPage() {
     if (currentPage >= _items.length - 1) return;
     currentPage++;
-    pageNumberController.add(currentPage);
+    topPageNumberController.add(currentPage);
     print('prev');
   }
 
   cancel() {
     _startAtX = null;
+    flyingPageController.add(null);
   }
 
   dispose() {
-    pageNumberController.close();
+    topPageNumberController.close();
+    flyingPageController.close();
   }
 }
 
+//todo вынести отсюда падинги в _HorizontalSwipeState
 class PageCard extends StatelessWidget {
   final Widget child;
 
